f31b759eb22f4d0b3f60532dfd1c97bce48f3dd9
Christopher-John Cassar <christopherjcassar@gmail.com>
2024-05-18 23:01:16 -0700

    [refactor] removed spaces

--- src/components/layouts/IssuesDashboard.tsx
+++ src/components/layouts/IssuesDashboard.tsx
@@ -6,9 +6,7 @@ export function IssuesDashboard() {
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
       <SideNavBar />
-
       <Header />
-
       <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
         <IssueTable />
       </div>
