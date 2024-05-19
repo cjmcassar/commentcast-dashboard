6a8b96b53b3035adaf744391041e74357c3878f1
Christopher-John Cassar <christopherjcassar@gmail.com>
2024-05-18 22:31:01 -0700

    [feat] added pwd reset page

---
+++ src/app/(auth)/pwd-reset/actions.ts
@@ -0,0 +1,33 @@
+'use server';
+
+import { createClient } from '@/utils/supabase/server';
+
+import { redirect } from 'next/navigation';
+
+const supabase = createClient();
+
+export async function resetPassword(email: string) {
+  if (process.env.IS_PRODUCTION) {
+    const { error } = await supabase.auth.resetPasswordForEmail(email, {
+      redirectTo: `${process.env.PRODUCTION_URL}/confirm-pwd-reset`,
+    });
+
+    if (error) {
+      console.error('Error resetting password:', error);
+      return { success: false, error: error.message };
+    }
+
+    return { success: true, error: null };
+  } else {
+    const { error } = await supabase.auth.resetPasswordForEmail(email, {
+      redirectTo: 'http://localhost:3000/confirm-pwd-reset',
+    });
+
+    if (error) {
+      console.error('Error resetting password:', error);
+      return { success: false, error: error.message };
+    }
+
+    return { success: true, error: null };
+  }
+}
---
+++ src/app/(auth)/pwd-reset/page.tsx
@@ -0,0 +1,13 @@
+import PwdReset from '@/components/layouts/auth/PwdReset';
+
+import React from 'react';
+
+type Props = {};
+
+export default function PwdResetPage(props: Props) {
+  return (
+    <div className="container">
+      <PwdReset />
+    </div>
+  );
+}
