5a5fe77280deb6aeb0644c4e29d6d81eebde0142
Christopher-John Cassar <christopherjcassar@gmail.com>
2024-05-24 16:25:51 -0700

    [feat] make everything copyable

--- src/components/layouts/issue-details/IssueDetails.tsx
+++ src/components/layouts/issue-details/IssueDetails.tsx
@@ -15,6 +15,7 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table';
+import { useToast } from '@/components/ui/use-toast';
 
 type Props = {
   slug: string;
@@ -50,6 +51,7 @@ class Issue {
 const IssueDetails = ({ slug }: Props) => {
   const [issue, setIssue] = useState<Issue>(new Issue());
   const supabase = createClient();
+  const { toast } = useToast();
 
   useEffect(() => {
     const fetchIssue = async () => {
@@ -72,6 +74,29 @@ const IssueDetails = ({ slug }: Props) => {
     fetchIssue();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [slug]);
+
+  const handleCopyToClipboard = (text: string, label: string) => {
+    let helperText = (
+      <>
+        The issue <b>{label}</b> has been copied to your clipboard
+      </>
+    );
+    navigator.clipboard
+      .writeText(text)
+      .then(() => {
+        toast({
+          title: 'Copied to clipboard',
+          description: helperText,
+        });
+      })
+      .catch((err) => {
+        console.error('Failed to copy text: ', err);
+        toast({
+          title: 'Error',
+          description: 'Failed to copy text to clipboard',
+        });
+      });
+  };
 
   return (
     <div>
@@ -98,43 +123,116 @@ const IssueDetails = ({ slug }: Props) => {
                 </TableHeader>
                 <TableBody>
                   <TableRow>
-                    <TableCell className="font-medium">
+                    <TableCell
+                      className="font-medium w-[100px] max-w-[100px] cursor-pointer"
+                      onClick={() =>
+                        handleCopyToClipboard(
+                          JSON.stringify(issue.logs),
+                          'Logs'
+                        )
+                      }
+                    >
+                      <strong>Logs:</strong>
                       {issue.logs && issue.logs.length > 0 ? (
-                        <ul>
+                        <ul className="space-y-1 pl-4">
                           {issue.logs.map((log, index) => (
-                            <li key={index}>
+                            <li
+                              key={index}
+                              className="pl-3 list-decimal break-words"
+                            >
                               <strong>{log.level}:</strong>{' '}
-                              {JSON.stringify(log.text)}
-                              {log.url && <a href={log.url}>Source</a>}
+                              {JSON.stringify(log.text).length > 300
+                                ? `${JSON.stringify(log.text).substring(0, 297)}...`
+                                : JSON.stringify(log.text)}
                             </li>
                           ))}
                         </ul>
-                      ) : null}
+                      ) : (
+                        <span className="pl-3 text-pretty">
+                          No logs detected.
+                        </span>
+                      )}
                     </TableCell>
                   </TableRow>
                   <TableRow>
-                    <TableCell>{issue.platform_arch}</TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell>{issue.platform_os}</TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell className="hidden md:table-cell">
-                      {issue.url}
+                    <TableCell
+                      className="cursor-pointer"
+                      onClick={() =>
+                        handleCopyToClipboard(
+                          issue.platform_arch,
+                          'Platform Arch'
+                        )
+                      }
+                    >
+                      <strong>Platform Arch:</strong> {issue.platform_arch}
                     </TableCell>
                   </TableRow>
                   <TableRow>
-                    <TableCell className="hidden md:table-cell">
+                    <TableCell
+                      className="cursor-pointer"
+                      onClick={() =>
+                        handleCopyToClipboard(issue.platform_os, 'Platform OS')
+                      }
+                    >
+                      <strong>Platform OS:</strong> {issue.platform_os}
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell
+                      className="hidden md:table-cell cursor-pointer"
+                      onClick={() =>
+                        handleCopyToClipboard(issue.url || '', 'Source URL')
+                      }
+                    >
+                      <strong>Source URL:</strong> {issue.url}
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell
+                      className="hidden md:table-cell cursor-pointer"
+                      onClick={() =>
+                        handleCopyToClipboard(
+                          `${issue.primary_display_dimensions?.primary_display_width} x ${issue.primary_display_dimensions?.primary_display_height}`,
+                          'Primary Display'
+                        )
+                      }
+                    >
+                      <strong>Primary Display:</strong>{' '}
                       {issue.primary_display_dimensions
                         ? `${issue.primary_display_dimensions.primary_display_width} x ${issue.primary_display_dimensions.primary_display_height}`
                         : 'N/A'}
                     </TableCell>
                   </TableRow>
                   <TableRow>
-                    <TableCell>{issue.browser_name}</TableCell>
+                    <TableCell
+                      className="cursor-pointer"
+                      onClick={() =>
+                        handleCopyToClipboard(
+                          issue.browser_name || '',
+                          'Browser Name'
+                        )
+                      }
+                    >
+                      <strong>Browser Name:</strong> {issue.browser_name}
+                    </TableCell>
                   </TableRow>
                   <TableRow>
-                    <TableCell>{issue.created_at}</TableCell>
+                    <TableCell
+                      className="cursor-pointer"
+                      onClick={() =>
+                        handleCopyToClipboard(issue.created_at || '', 'Date')
+                      }
+                    >
+                      <strong>Date:</strong>{' '}
+                      {new Date(issue.created_at).toLocaleDateString('en-UK', {
+                        year: 'numeric',
+                        month: 'long',
+                        day: 'numeric',
+                        hour: 'numeric',
+                        minute: 'numeric',
+                        hour12: true,
+                      })}
+                    </TableCell>
                   </TableRow>
                 </TableBody>
               </Table>
