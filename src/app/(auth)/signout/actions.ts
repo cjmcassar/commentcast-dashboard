2167667b0449aacca68936bd5190992ebc412b56
Christopher-John Cassar <christopherjcassar@gmail.com>
2024-05-24 15:11:25 -0700

    *

--- src/app/(auth)/signout/actions.ts
+++ src/app/(auth)/signout/actions.ts
@@ -2,9 +2,6 @@
 
 import { createClient } from '@/utils/supabase/server';
 
-import { revalidatePath } from 'next/cache';
-import { redirect } from 'next/navigation';
-
 export async function signOut() {
   const supabase = createClient();
 
