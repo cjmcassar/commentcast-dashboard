27f4c39cfbce9eee1f5bafc76a255d1b20616152
Christopher-John Cassar <christopherjcassar@gmail.com>
2024-05-25 11:57:24 -0700

    [refactor] changed dev env var

--- src/app/(auth)/pwd-reset/actions.ts
+++ src/app/(auth)/pwd-reset/actions.ts
@@ -7,7 +7,7 @@ import { redirect } from 'next/navigation';
 const supabase = createClient();
 
 export async function resetPassword(email: string) {
-  if (process.env.IS_PRODUCTION) {
+  if (!process.env.IS_DEV) {
     const { error } = await supabase.auth.resetPasswordForEmail(email, {
       redirectTo: `${process.env.PRODUCTION_URL}/confirm-pwd-reset`,
     });
