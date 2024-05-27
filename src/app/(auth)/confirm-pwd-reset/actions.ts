d5d31c90c29435b584ccb83818d03f83b5e06a17
Christopher-John Cassar <christopherjcassar@gmail.com>
2024-05-27 16:25:56 -0700

    [fix] accept auth code then proceed to reset password

--- src/app/(auth)/confirm-pwd-reset/actions.ts
+++ src/app/(auth)/confirm-pwd-reset/actions.ts
@@ -2,10 +2,24 @@
 
 import { createClient } from '@/utils/supabase/server';
 
+import { redirect } from 'next/navigation';
+
 const supabase = createClient();
 
-export async function confirmPasswordReset(newPassword: string) {
+export async function confirmPasswordReset(
+  newPassword: string,
+  searchParamsCode: string
+) {
   try {
+    if (searchParamsCode) {
+      const supabase = createClient();
+      const { error } =
+        await supabase.auth.exchangeCodeForSession(searchParamsCode);
+
+      if (error) {
+        return redirect(`/pwd-reset`);
+      }
+    }
     // Verify the code and email (this part depends on your backend implementation)
     // Assuming the code is valid and email is verified, proceed to update the password
     const { error } = await supabase.auth.updateUser({
