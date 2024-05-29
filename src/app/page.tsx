de59f74c31e8a6536bd1515b1f1637b8ed3577c3
Christopher-John Cassar <christopherjcassar@gmail.com>
2024-05-29 15:15:07 -0700

    *

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,23 +5,6 @@ import { redirect } from 'next/navigation';
 
 import { TooltipProvider } from '@/components/ui/tooltip';
 
-// Authentication and User Management
-// 1. Add in dev env so that the CE can test locally easily
-// 2.
-
-// Issue Management
-// 1.
-
-// 2.
-// 3. TODO: require email confirmation of the person you're sharing with
-// 4. todo: Add multimodal AI to digest issues and summarize them
-
-// User Interface Enhancements
-// 1. todo: Add a basic settings page
-// 2.
-// 3.
-// 4.
-
 export default async function Home() {
   const supabase = createClient();
 
